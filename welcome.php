4d65c335057eaff6d2ca2dc9ad0c3c7abb23660a
sheilazaguirre <sheila.zaguirre@benilde.edu.ph>
2017-01-24 04:52:02 +0800

    found errors and fix it

--- welcome.php
+++ welcome.php
@@ -30,14 +30,14 @@
 
 	</head>
 	<body>
-		<section id="header" class="appear"></section>
+		<section id="section-header" class="appear"></section>
 			<div class="navbar navbar-fixed-top" role="navigation" data-0="line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);" data-300="line-height:60px; height:60px; background-color:rgba(0,0,0,1);">
 			 	<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 							<span class="fa fa-bars color-white"></span>
 						</button>
-						<h1><a class="navbar-brand" href="welcome.php" data-0="line-height:90px;" data-300="line-height:50px;">			HOME
+						<h1><a class="navbar-brand" href="#section-header" data-0="line-height:90px;" data-300="line-height:50px;">			HOME
 						</a></h1>
 					</div>
 					<div class="navbar-collapse collapse">
