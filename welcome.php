4e9a24c2dac476802b258eed50d928926e28cd73
sheilazaguirre <sheila.zaguirre@benilde.edu.ph>
2017-01-24 04:32:30 +0800

    Finish
    add contact and footer

--- welcome.php
+++ welcome.php
@@ -30,7 +30,7 @@
 
 	</head>
 	<body>
-		<section id="header" class="appear">
+		<section id="header" class="appear"></section>
 			<div class="navbar navbar-fixed-top" role="navigation" data-0="line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);" data-300="line-height:60px; height:60px; background-color:rgba(0,0,0,1);">
 			 	<div class="container">
 					<div class="navbar-header">
@@ -48,7 +48,7 @@
 					</div>
 				</div>
 			</div>
-		</section>
+
 			<section class="featured">
 				<div class="container"> 
 					<div class="row mar-bot40">
@@ -86,7 +86,7 @@
 				</div>	
 			</section>
 
-		<section id="section-studentinfo" class="section appear clearfix">
+		<section id="section-studentinfo" class="section">
 			<div class="container">
 
 				<div class="row mar-bot40">
@@ -129,22 +129,98 @@
             	</div>
 			</div>	
 		</section>
-	<!--<body style="height:100%">
-		<div class="container">
-			<div style="height:100vh; display: flex;justify-content: center;align-items: center;">
-				<div class="jumbotron" style="text-align:center">
-					<h1>Welcome, <?=$lastName?> <?=$firstName?>! </h1>
-					<div class="row">
-						<div class="col-xs-6" style="text-align:left; font-size:120%">
-							<?=$studentNo?><br/>
-							<?=$emailAddress?><br/>
-							<?=$password?><br/>
-							<?=$birthDate?><br/>
-							<?=date('c')?><br/>
+
+		<section id="section-contact" class="section appear clearfix">
+			<div class="container">
+				
+				<div class="row mar-bot40">
+					<div class="col-md-offset-3 col-md-6">
+						<div class="section-header">
+							<i class="fa fa-phone fa-3x mar-bot20"></i>
+							<h2 class="section-heading animated" data-animation="bounceInUp">Contact us</h2>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam.</p>
 						</div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="cform" id="contact-form">
+							<div id="sendmessage">
+								 Your message has been sent. Thank you!
+							</div>
+                            <div id="errormessage"></div>
+							<form action="" method="post" role="form" class="contactForm">
+							  <div class="form-group">
+								<label for="name">Your Name</label>
+								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+								<div class="validation"></div>
+							  </div>
+							  <div class="form-group">
+								<label for="email">Your Email</label>
+								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+								<div class="validation"></div>
+							  </div>
+							  <div class="form-group">
+								<label for="subject">Subject</label>
+								<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+								<div class="validation"></div>
+							  </div>
+							  <div class="form-group">
+								<label for="message">Message</label>
+								<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us"></textarea>
+								<div class="validation"></div>
+							  </div>
+							  
+							  <button type="submit" class="btn btn-theme pull-left">SEND MESSAGE</button>
+							</form>
+
+						</div>
+					</div>
+					<!-- ./span12 -->
+				</div>
+				
 			</div>
-		</div>
-	</body>-->
+		</section>
+
+		<section id="footer" class="section footer">
+			<div class="container">
+				<div class="row animated opacity mar-bot20" data-andown="fadeIn" data-animation="animation">
+					<div class="col-sm-12 align-center">
+                    	<ul class="social-network social-circle">
+                        	<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+                        	<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        	<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                        	<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                        	<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    	</ul>				
+					</div>
+				</div>
+
+				<div class="row align-center copyright">
+					<div class="col-sm-12"><p>Copyright &copy; Amoeba</p>
+                    </div>
+				</div>
+			</div>
+
+		</section>
+
+	<a href="#header" class="scrollup"><i class="fa fa-chevron-up"></i></a>	
+
+	<script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.nicescroll.min.js"></script>
+	<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="js/skrollr.min.js"></script>		
+	<script src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+	<script src="js/jquery.localscroll-1.2.7-min.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/jquery.appear.js"></script>
+    <script src="js/main.js"></script>
+    <script src="contactform/contactform.js"></script>
+    
+	</body>
 </html>
